984c870740f4bc700f1abd81a19a10e2e861c68f
lspongebobjh <lspongebobjh@gmail.com>
2024-04-14 06:22:34 +0000

    update pos_e_w, protein-debug-3

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug-2
+python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug-3 model.pos_e_w=100
