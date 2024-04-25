6628913ae47be36c7ffdd768194e8f1c547b17d6
lspongebobjh <lspongebobjh@gmail.com>
2024-04-25 05:47:14 +0000

    protein-debug-4

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug-3 model.pos_e_w=100
-python3 scripts/draw_reverse_metrics.py --path /home/jiahang/DiGress/chain_results/2024-04-14/06-23-23-protein-debug-3/protein-debug-3 --stage valid test
+python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug-4 train.n_epochs=50
+
+# python3 scripts/draw_reverse_metrics.py \
+# --path /home/jiahang/DiGress/chain_results/2024-04-25/04-51-01-protein-debug/protein-debug \
+# --stage valid test --epoch_end 29
