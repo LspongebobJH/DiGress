a9692cbe7b18f50eb9401a8ed1c5397f6925a686
lspongebobjh <lspongebobjh@gmail.com>
2024-04-25 03:56:57 +0000

    delete unnecessary loss items: x and y

--- src/metrics/train_metrics.py
+++ src/metrics/train_metrics.py
@@ -63,9 +63,7 @@ class TrainLossDiscrete(nn.Module):
     """ Train with Cross entropy"""
     def __init__(self, lambda_train, pos_e_w):
         super().__init__()
-        self.node_loss = CrossEntropyMetric()
         self.edge_loss = CrossEntropyMetric(pos_e_w)
-        self.y_loss = CrossEntropyMetric()
         self.lambda_train = lambda_train
 
     def forward(self, masked_pred_X, masked_pred_E, pred_y, true_X, true_E, true_y, log: bool):
@@ -77,46 +75,33 @@ class TrainLossDiscrete(nn.Module):
         true_E : tensor -- (bs, n, n, de)
         true_y : tensor -- (bs, )
         log : boolean. """
-        true_X = torch.reshape(true_X, (-1, true_X.size(-1)))  # (bs * n, dx)
         true_E = torch.reshape(true_E, (-1, true_E.size(-1)))  # (bs * n * n, de)
-        masked_pred_X = torch.reshape(masked_pred_X, (-1, masked_pred_X.size(-1)))  # (bs * n, dx)
         masked_pred_E = torch.reshape(masked_pred_E, (-1, masked_pred_E.size(-1)))   # (bs * n * n, de)
 
         # Remove masked rows
-        mask_X = (true_X != 0.).any(dim=-1)
         mask_E = (true_E != 0.).any(dim=-1)
-
-        flat_true_X = true_X[mask_X, :]
-        flat_pred_X = masked_pred_X[mask_X, :]
-
         flat_true_E = true_E[mask_E, :]
         flat_pred_E = masked_pred_E[mask_E, :]
 
-        loss_X = self.node_loss(flat_pred_X, flat_true_X, loss_type = 'node') if true_X.numel() > 0 else 0.0
         loss_E = self.edge_loss(flat_pred_E, flat_true_E, loss_type = 'edge') if true_E.numel() > 0 else 0.0
-        loss_y = self.y_loss(pred_y, true_y, loss_type = 'graph') if true_y.numel() > 0 else 0.0
 
         if log:
-            to_log = {"train_loss/batch_CE": (loss_X + loss_E + loss_y).detach(),
-                      "train_loss/X_CE": self.node_loss.compute() if true_X.numel() > 0 else -1,
+            to_log = {"train_loss/batch_CE": loss_E.detach(),
                       "train_loss/E_CE": self.edge_loss.compute() if true_E.numel() > 0 else -1,
-                      "train_loss/y_CE": self.y_loss.compute() if true_y.numel() > 0 else -1}
+                }
             if wandb.run:
                 wandb.log(to_log, commit=True)
-        return loss_X + self.lambda_train[0] * loss_E + self.lambda_train[1] * loss_y
+        # return loss_X + self.lambda_train[0] * loss_E + self.lambda_train[1] * loss_y
+        return self.lambda_train[0] * loss_E
 
     def reset(self):
-        for metric in [self.node_loss, self.edge_loss, self.y_loss]:
-            metric.reset()
+        self.edge_loss.reset()
 
     def log_epoch_metrics(self):
-        epoch_node_loss = self.node_loss.compute() if self.node_loss.total_samples > 0 else -1
         epoch_edge_loss = self.edge_loss.compute() if self.edge_loss.total_samples > 0 else -1
-        epoch_y_loss = self.train_y_loss.compute() if self.y_loss.total_samples > 0 else -1
-
-        to_log = {"train_epoch/x_CE": epoch_node_loss,
-                  "train_epoch/E_CE": epoch_edge_loss,
-                  "train_epoch/y_CE": epoch_y_loss}
+        to_log = {
+            "train_epoch/E_CE": epoch_edge_loss
+        }
         if wandb.run:
             wandb.log(to_log, commit=False)
 
