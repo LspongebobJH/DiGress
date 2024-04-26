08450526e391ecc4c26a71c44dbd5aa80acc19de
lspongebobjh <lspongebobjh@gmail.com>
2024-04-26 13:46:19 +0000

    TODO: bug, L44 protein_metrics.py

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 # python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug-4 train.n_epochs=50
 
 python3 scripts/draw_reverse_metrics.py \
---path /home/jiahang/DiGress/chain_results/2024-04-25/05-45-36-protein-debug-4/protein-debug-4 \
---stage valid test --epoch_end 49
+--path /home/jiahang/DiGress/chain_results/2024-04-26/13-19-47-protein-debug/protein-debug \
+--stage valid test --epoch_end 19
