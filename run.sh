8e817213252fba3b15d5e39f4b8217d90ed7a898
lspongebobjh <lspongebobjh@gmail.com>
2024-05-01 10:32:28 +0000

    protein-1 infer

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-python3 src/main.py +experiment=protein dataset=protein general.name=protein-debug train.n_epochs=50
+if [ "$1" == "train" ]; then
+    # train
+    echo 'train'
+    python3 src/main.py +experiment=protein general.name=protein-1 train.n_epochs=20 dataset.slope=50
 
-# python3 scripts/draw_reverse_metrics.py \
-# --path /home/jiahang/DiGress/chain_results/2024-04-26/13-19-47-protein-debug/protein-debug \
-# --stage valid test --epoch_end 19
+elif [ "$1" == "infer" ]; then
+    # infer
+    echo 'infer'
+    python3 src/main.py +experiment=protein general.infer=True \
+    'general.test_only=/home/jiahang/DiGress/outputs/2024-05-01/07-25-48-protein-1/checkpoints/protein-1/epoch\=9.ckpt'
+
+elif [ "$1" == "draw" ]; then
+    # draw lp metrics of reverse process
+    echo 'draw'
+    python3 scripts/draw_reverse_metrics.py \
+    --path /home/jiahang/DiGress/chain_results/2024-05-01/07-25-48-protein-1/protein-1 \
+    --stage valid test --epoch_end 19
+
+else
+    echo "Unrecognized arg $1"
+fi
